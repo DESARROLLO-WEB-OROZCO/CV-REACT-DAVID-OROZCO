c5be8ed086b2e5cf990176b23765577809fd662e
david alejandro orozco henao <davidalejandroorozcohenao@gmail.com>
2025-11-14 13:57:22 -0500

    feat: App.jsx estructurado con componentes anidados

---
+++ src/components/Educacion.jsx
@@ -0,0 +1,17 @@
+function Educacion() {
+  return (
+    <section>
+      <h2>Educación</h2>
+
+      <ul>
+        <li>Técnico en Programación – SENA (2023)</li>
+        <li>Curso de React – Udemy (2024)</li>
+        <li>Curso de Git y GitHub – Platzi (2024)</li>
+        <li>Diplomado en Desarrollo Web – SENA (2023)</li>
+      </ul>
+    </section>
+  );
+}
+
+export default Educacion;
+
