b914a830ce9d5dd98431ee13ca68a0da3e1ae37c
david alejandro orozco henao <davidalejandroorozcohenao@gmail.com>
2025-11-14 13:47:04 -0500

    feat: componente CabeceraCV con datos personales actualizados

---
+++ src/components/CabeceraCV.jsx
@@ -0,0 +1,14 @@
+function CabeceraCV() {
+  return (
+    <header>
+      <h1>DAVID OROZCO</h1>
+      <h2>DESARROLLADOR FRONT-END JR.</h2>
+
+      <p>Tel: 3040000000</p>
+      <p>Correo: david@example.com</p>
+      <p>Ciudad: Colombia</p>
+    </header>
+  );
+}
+
+export default CabeceraCV;
